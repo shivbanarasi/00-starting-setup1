d7b3a0b7e4202c4a34811cda441cd9d9c40d6efe
shivbanarasi <shivbanarasi0542@gmail.com>
2023-03-01 07:27:53 +0530

    edit and delete

--- models/cart.js
+++ models/cart.js
@@ -28,13 +28,32 @@ module.exports=class Cart{
             }else{
                 updatedProduct={id:id,qty:1};
                 cart.products=[...cart.products,updatedProduct];
-            }
+            } 
             cart.totalPrice=cart.totalPrice+ +productPrice;
             fs.writeFile(p,JSON.stringify(cart),err=>{
                 console.log(err);
             })
         });
 
+    }
+    static deleteProduct(id,productPrice){
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return;
+            }
+            const updatedCart={...JSON.parse(fileContent)};
+            const product=updatedCart.products.find(prod=>prod.id===id);
+            const productQty=product.qty;
+            updatedCart.products=updatedCart.products.filter(
+                prod=>prod.id !== id
+            );
+            updatedCart.totalPrice=
+            updatedCart.totalPrice-productPrice*productQty;
+
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                console.log(err);
+            })
+        })
     }
 
 }
